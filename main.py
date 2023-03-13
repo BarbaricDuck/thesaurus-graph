df9807f7cb79ea8103da8360a7f72a5376824907
Ben Groves <ben_groves2004@outlook.com>
2023-03-13 13:12:29 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,31 +17,32 @@ class Graph:
         self.nodes[node] = neighbours
     
     def get_path(self, start, end):
-        self.paths = [[start]]
+        self.paths = [[[start]]]
         self.visited = set()
 
         while end not in self.visited:
-            old_len = len(self.paths)
-            for path in self.paths:
+            print(len(self.paths), len(self.paths[-1]))
+            old_len = len(self.paths[-1])
+            self.paths.append([])
+            for path in self.paths[-2]:
                 self.next_depth(path)
-            if len(self.paths) == old_len:
+            if len(self.paths[-1]) == old_len:
                 break
             
-        for path in self.paths:
+        for path in self.paths[-1]:
             if end in path:
                 return path
-        return [None]
+        return ["Not Connected"]
             
     def next_depth(self, path):
         for neighbour in filter(lambda x: x not in self.visited, self.nodes[path[-1]]):
-            self.paths.append(path + [neighbour])
+            self.paths[-1].append(path + [neighbour])
             self.visited.add(neighbour)
 
 def random_word_generator():
     with open('words_big.csv') as file:
         keys = [i[0] for i in csv.reader(file)]
         return random.choice(keys)
-
     
 def main():
     graph = Graph()
@@ -49,18 +50,27 @@ def main():
         csv_file = [x for x in csv.reader(file)]
         keys = set([i[0] for i in csv_file])
         for line in csv_file:
-            # print(line[0], list(filter(lambda x: x in keys, line[1:])))
             graph.add_node(line[0], list(filter(lambda x: x in keys, line[1:])))
             
     print(f'Graph created, size: {len(graph.nodes)}')
     try:
-        start = sys.argv[1]
+        if (arg_1:=sys.argv[1]) in graph.nodes:
+            start = arg_1
+        else:
+            print(f'{arg_1} not in word list')
+            start = random_word_generator() 
     except IndexError:
         start = random_word_generator()
+
     try:
-        end = sys.argv[2]
+        if (arg_2:=sys.argv[2]) in graph.nodes:
+            end = arg_2
+        else:
+            print(f'{arg_2} not in word list')
+            end = random_word_generator() 
     except IndexError:
         end = random_word_generator()
+
     print(f'Searching for {start} to {end}')
     print(f'{" -> ".join(graph.get_path(start, end))}')
 
